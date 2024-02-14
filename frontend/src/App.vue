b871cf6232e16733e72ef5296970ad22a5321624
EmaMaknae <emanuellyinacio99@gmail.com>
2024-02-13 22:20:23 -0300

    estilização da página

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <div class="img">
+      <img alt="secureshopping" src="./assets/secureshopping.png">
+    </div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -17,12 +19,19 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+.img img {
+  height: 200px;
+  width: 100%;
+}
+
 </style>
--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,29 +1,28 @@
 <template>
   <div class="hello">
-    <h1>Lista de Marcas:</h1>
-    <ul>
-      <li v-for="marca in marcas" :key="marca.id">
-        {{ marca.nome }}
-      </li>
-    </ul>
-
-    <h1>Lista de Produtos:</h1>
-    <ul>
-      <li v-for="produto in produtos" :key="produto.id">
-        {{ produto.nome }} ({{ produto.marca_nome }})
-      </li>
-    </ul>
-
-    <h2>Adicionar Novo Produto:</h2>
+    <h2 class="subtitle " >Adicionar Novo Produto:</h2>
     <form @submit.prevent="adicionarProduto">
-      <label for="novoProduto">Novo Produto:</label>
-      <input type="text" id="novoProduto" v-model="novoProdutoNome">
-      <label for="marcaSelecionada">Marca:</label>
+      <label element class="produto" for="novoProduto">Novo Produto:</label>
+      <input  type="text" id="novoProduto" v-model="novoProdutoNome">
+      <label  element class="marca" for="marcaSelecionada">Marca:</label>
       <select v-model="marcaSelecionada">
         <option v-for="marca in marcas" :key="marca.id" :value="marca.id">{{ marca.nome }}</option>
       </select>
       <button type="submit">Adicionar Produto</button>
     </form>
+    <h1 class="title">Lista de Marcas:</h1>
+    <ul>
+      <li v-for="marca in marcas" :key="marca.id" >
+        {{ marca.nome }}
+      </li>
+    </ul>
+
+    <h1 class="title">Lista de Produtos:</h1>
+    <ul>
+      <li v-for="produto in produtos" :key="produto.id">
+        {{ produto.nome }} ({{ produto.marca_nome }})
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -82,4 +81,43 @@ export default {
 </script>
 
 <style scoped>
+ul {
+  list-style: none;
+}
+input, select{
+  padding: 8px;
+  border: 1px solid #342B4B;
+  border-radius: 4px;
+}
+button {
+  padding: 8px;
+  background-color: #FDBC2C;
+  color: #342B4B;
+  margin-left: 1rem;
+  border: 2px dotted #FFF013;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
+button:hover {
+  background-color: #342B4B;
+  color: #FDBC2C;
+}
+.title {
+  color:#342B4B;
+
+}
+.subtitle {
+  color: #342B4B;
+}
+.produto {
+font-weight: bold;
+color: #342B4B;
+padding: 8px;
+}
+.marca {
+  font-weight: bold;
+  color: #342B4B;
+  padding:8px;
+}
 </style>
